98e4ed93890d7e835ea398ead5ccc2c7b0ee6cae
Fery Wardiyanto <ferywardiyanto@gmail.com>
2025-10-15 11:10:35 +0700

    Add assertion covering `users` and `sessions` creation
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- test/routes/auth.route.test.ts
+++ test/routes/auth.route.test.ts
@@ -2,6 +2,8 @@ import { afterAll, describe, expect, it } from 'bun:test'
 import { ENV } from '~/config'
 import { db } from '~/database'
 import { accounts, sessions, users } from '~/database/schemas'
+import { sessionRepository, userRepository } from '~/modules/auth/repositories'
+import type { User } from '~/modules/auth/types'
 import { authRoute } from '~/routes/auth.route'
 
 describe('Auth Routes', () => {
@@ -25,7 +27,9 @@ describe('Auth Routes', () => {
     expect(response.status).toBe(200)
     expect(response.headers.get('Content-Type')).toBe('application/json')
 
-    expect(await response.json()).toEqual({
+    const body = (await response.json()) as { token: string; user: User }
+
+    expect(body).toEqual({
       token: expect.any(String),
       user: expect.objectContaining({
         id: expect.any(String),
@@ -36,5 +40,14 @@ describe('Auth Routes', () => {
         updatedAt: expect.any(String),
       }),
     })
+
+    // Ensure that the anonymous user exists
+    expect(await userRepository.exists(body.user.id)).toBeTrue()
+
+    // Ensure that the anonymous session token is valid
+    const sessions = await sessionRepository.getAllByUserId(body.user.id)
+    expect(sessions).toBeArrayOfSize(1)
+    expect(sessions[0]?.token).toBe(body.token)
+    expect(sessions[0]?.revoked).toBeFalse()
   })
 })
