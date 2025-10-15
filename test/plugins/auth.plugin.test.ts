58ad8b93fac29bf3fcbce80ba8f3b2d22bb5379b
Fery Wardiyanto <ferywardiyanto@gmail.com>
2025-10-15 09:00:41 +0700

    Ensure `user` instance got passed to request handler
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- test/plugins/auth.plugin.test.ts
+++ test/plugins/auth.plugin.test.ts
@@ -1,29 +1,63 @@
-import { describe, expect, it } from "bun:test";
-import Elysia from "elysia";
-import { auth, authPlugin } from "~/plugins/auth.plugin";
+import type { UserWithAnonymous } from 'better-auth/plugins'
+import { afterAll, afterEach, describe, expect, it, mock } from 'bun:test'
+import Elysia from 'elysia'
+import { db } from '~/database'
+import { accounts, sessions, users } from '~/database/schemas'
+import { auth, authPlugin } from '~/plugins/auth.plugin'
 
 describe('Auth Plugin', () => {
-  const authApp = new Elysia()
-    .use(authPlugin)
-    .get('', () => 'Auth')
+  const handler = mock(() => 'Auth')
+  const authApp = new Elysia().use(authPlugin).get('', handler)
+
+  afterEach(() => {
+    handler.mockClear()
+  })
+
+  afterAll(async () => {
+    await db.delete(accounts)
+    await db.delete(sessions)
+    await db.delete(users)
+  })
 
   describe('Middleware', () => {
     it('should returns 404 status when no authorization header', async () => {
-      const response = await authApp.handle(
-        new Request('http://localhost')
-      )
+      const response = await authApp.handle(new Request('http://localhost'))
 
       expect(response.status).toBe(404)
+      expect(handler).not.toHaveBeenCalled()
     })
 
     it('should returns 401 status when credential is invalid', async () => {
       const response = await authApp.handle(
         new Request('http://localhost', {
-          headers: { 'authorization': 'Bearer invalid' }
-        })
+          headers: { authorization: 'Bearer invalid' },
+        }),
       )
 
       expect(response.status).toBe(401)
+      expect(handler).not.toHaveBeenCalled()
+    })
+
+    it('should receive user object when authenticated', async () => {
+      const authenticated = await auth.api.signInAnonymous()
+      const response = await authApp.handle(
+        new Request('http://localhost', {
+          headers: { authorization: `Bearer ${authenticated?.token}` },
+        }),
+      )
+
+      expect(response.status).toBe(200)
+      expect(handler).toHaveBeenCalled()
+
+      // Ensure `user` is get passed to request context
+      const [ctx] =
+        handler.mock.lastCall || ([{}] as [{ user?: UserWithAnonymous }])
+      expect(ctx).toContainKey('user')
+
+      // Ensure instance of `user` is expected to be Anonymous
+      const user = ctx?.user as UserWithAnonymous
+      expect(user.isAnonymous).toBeTrue()
+      expect(user.emailVerified).toBeFalse()
     })
   })
 })
