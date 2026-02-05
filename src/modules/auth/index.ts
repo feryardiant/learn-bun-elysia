cd78a25204cba3f9f31fe5ada4c794c8ae305d6f
Fery Wardiyanto <ferywardiyanto@gmail.com>
2026-02-05 08:55:37 +0700

    chore: rearrange modules entrypoints
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- src/modules/auth/index.ts
+++ src/modules/auth/index.ts
@@ -1,5 +1,10 @@
+export * from './schemas/accounts.schema'
+export * from './schemas/sessions.schema'
+export * from './schemas/users.schema'
+export * from './schemas/verifications.schema'
+
 export * from './repositories/account.repository'
 export * from './repositories/session.repository'
 export * from './repositories/user.repository'
-export * from './schemas'
+
 export * from './types'
--- src/modules/feeds/repositories/index.ts
+++ src/modules/feeds/repositories/index.ts
@@ -1,9 +1,2 @@
-import { db } from '~/plugins/db.plugin'
-import { CommentRepository } from './comment.repository'
-import { PostRepository } from './post.repository'
-
-export { CommentRepository, PostRepository }
-
-export const postRepository = new PostRepository(db)
-
-export const commentRepository = new CommentRepository(db)
+export * from './comment.repository'
+export * from './post.repository'
