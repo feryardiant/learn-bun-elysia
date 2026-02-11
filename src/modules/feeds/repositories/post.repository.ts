fc5ef224fe09bb88c7351435040baaa7b7e08ad1
Fery Wardiyanto <ferywardiyanto@gmail.com>
2026-02-11 12:04:36 +0700

    chore: implements `Paginable` interface to `PostRepository`
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- src/modules/feeds/repositories/post.repository.ts
+++ src/modules/feeds/repositories/post.repository.ts
@@ -1,8 +1,50 @@
 import type { AppDatabase } from '~/plugins/database.plugin'
-import type { Post } from '../types'
+import type { FeedQuery, Post } from '../types'
+import { type Paginable } from '~/utils/pagination.util'
+import { posts } from '../schemas/posts.schema'
+import { and, between, eq, gt, lt, or, SQL } from 'drizzle-orm'
+import { getRange } from '~/utils/filters.util'
 
-export class PostRepository {
+export class PostRepository implements Paginable {
   constructor(private readonly db: AppDatabase) {}
+
+  async getNextToken(
+    query: FeedQuery,
+    entry?: Post,
+  ): Promise<[number, string] | null> {
+    if (!entry) return null
+
+    const filters = or(
+      lt(posts.createdAt, entry.createdAt),
+      and(eq(posts.createdAt, entry.createdAt), lt(posts.id, entry.id)),
+    )
+
+    const entries = await this.db.$count(
+      posts.id,
+      and(filters, ...this.buildFilters(query)),
+    )
+
+    return entries > 0 ? [new Date(entry.createdAt).getTime(), entry.id] : null
+  }
+
+  async getPrevToken(
+    query: FeedQuery,
+    entry?: Post,
+  ): Promise<[number, string] | null> {
+    if (!entry) return null
+
+    const filters = or(
+      gt(posts.createdAt, entry.createdAt),
+      and(eq(posts.createdAt, entry.createdAt), gt(posts.id, entry.id)),
+    )
+
+    const entries = await this.db.$count(
+      posts.id,
+      and(filters, ...this.buildFilters(query)),
+    )
+
+    return entries > 0 ? [new Date(entry.createdAt).getTime(), entry.id] : null
+  }
 
   async getAll(): Promise<Post[]> {
     const items = await this.db.query.posts.findMany({
@@ -22,5 +64,17 @@ export class PostRepository {
     }
 
     return item
+  }
+
+  private buildFilters({ date_range }: FeedQuery) {
+    const filters: SQL[] = []
+
+    if (date_range) {
+      const today = new Date()
+
+      filters.push(between(posts.createdAt, getRange(date_range, today), today))
+    }
+
+    return filters
   }
 }
