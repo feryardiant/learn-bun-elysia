a9cd2d346780c49785f2951363144a6dc7ae202a
Fery Wardiyanto <ferywardiyanto@gmail.com>
2026-02-13 19:16:55 +0700

    chore: introduce new error for missing config
    Will standardize other errors later
    
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- src/utils/errors.util.ts
+++ src/utils/errors.util.ts
@@ -1,5 +1,9 @@
 import { NotFoundError as ElysiaNotFoundError } from 'elysia'
 
+const codes = {
+  MISSING_CONFIG: 'MISSING_CONFIG',
+} as const
+
 export class AuthenticationError extends Error {
   static readonly code = 'AUTHENTICATION'
   readonly status = 401
@@ -23,9 +27,15 @@ export class InvalidParamError extends Error {
   readonly status = 422
 }
 
+export class MissingConfigError extends Error {
+  readonly code = codes.MISSING_CONFIG
+  readonly status = 500
+}
+
 export default {
   [AuthenticationError.code]: AuthenticationError,
   [AuthorizationError.code]: AuthorizationError,
   [NotFoundError.code]: NotFoundError,
   [InvalidParamError.code]: InvalidParamError,
+  [codes.MISSING_CONFIG]: MissingConfigError,
 }
