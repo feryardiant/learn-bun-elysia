add18040fb30d42f50a903549f9c3ebea5b0555c
Fery Wardiyanto <ferywardiyanto@gmail.com>
2026-02-15 06:44:38 +0700

    chore: workaround with otel plugin initiation
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- src/plugins/otel.plugin.ts
+++ src/plugins/otel.plugin.ts
@@ -1,51 +1,45 @@
 import { getCurrentSpan, opentelemetry } from '@elysiajs/opentelemetry'
-import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http'
-import { OTLPLogExporter } from '@opentelemetry/exporter-logs-otlp-http'
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-proto'
 import {
   envDetector,
   hostDetector,
   processDetector,
   resourceFromAttributes,
 } from '@opentelemetry/resources'
-import { logs, tracing } from '@opentelemetry/sdk-node'
+import {
+  ATTR_SERVICE_NAME,
+  ATTR_SERVICE_VERSION,
+} from '@opentelemetry/semantic-conventions'
 import { ENV } from '~/config'
-
-const headers = {
-  'content-type': 'application/json',
-}
 
 const traceExporter = new OTLPTraceExporter({
   url: `${ENV.OTEL_EXPORTER_OTLP_ENDPOINT}/v1/traces`,
-  headers,
-})
-
-const logExporter = new OTLPLogExporter({
-  url: `${ENV.OTEL_EXPORTER_OTLP_ENDPOINT}/v1/logs`,
-  headers,
 })
 
 const resource = resourceFromAttributes({
   'deployment.environment.name': ENV.NODE_ENV,
-  'deployment.name': ENV.APP_NAME,
-  'service.name': ENV.APP_NAME,
-  'service.version': ENV.APP_VERSION,
-})
-
-const logProvider = new logs.LoggerProvider({
-  resource,
-  processors: [new logs.BatchLogRecordProcessor(logExporter)],
+  [ATTR_SERVICE_NAME]: ENV.APP_NAME,
+  [ATTR_SERVICE_VERSION]: ENV.APP_VERSION,
 })
 
 export const otelPlugin = opentelemetry({
   serviceName: ENV.APP_NAME,
   autoDetectResources: true,
-  checkIfShouldTrace: (req) => {
+  checkIfShouldTrace(req) {
+    const url = new URL(req.url)
+    const userAgent = req.headers.get('user-agent')
+
+    if (userAgent) {
+      // Don't trace request from health check
+      return !(url.pathname === '/health' && userAgent.startsWith('Bun'))
+    }
+
     return true
   },
   resource,
   resourceDetectors: [envDetector, hostDetector, processDetector],
-  spanProcessors: [new tracing.BatchSpanProcessor(traceExporter)],
-}).derive({ as: 'global' }, function requestInfo({ path, request }) {
+  traceExporter,
+}).derive({ as: 'global' }, function RequestInfo({ path, request }) {
   const sessionId = request.headers.get('x-session-id') || crypto.randomUUID()
   const currentSpan = getCurrentSpan()
 
