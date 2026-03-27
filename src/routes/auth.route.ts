db0fb8f44fd51cc2dc05a33335d2deb4cc76914f
Fery Wardiyanto <ferywardiyanto@gmail.com>
2026-03-28 06:25:44 +0700

    chore(routes): fix conflicting routes between auth and static plugin
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,8 +1,9 @@
 import { Elysia } from 'elysia'
 import { auth } from '~/plugins/auth.plugin'
 
-export const authRoute = new Elysia({ name: 'auth-route' }).mount(
-  async function HandleAuth(req) {
-    return await auth.handler(req)
-  },
-)
+export const authRoute = new Elysia({
+  name: 'auth-route',
+  prefix: auth.options.basePath,
+}).mount(async function HandleAuth(req) {
+  return await auth.handler(req)
+})
