fa74c19beb0393a95a160bc381df64f95d478680
Fery Wardiyanto <ferywardiyanto@gmail.com>
2026-03-30 00:02:39 +0700

    chore: rearrange code structure & prepare for next
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- src/utils/otel.util.ts
+++ src/utils/otel.util.ts
@@ -1,4 +1,5 @@
 import {
+  context,
   SpanKind,
   SpanStatusCode,
   trace,
@@ -8,6 +9,104 @@ import {
 import { ATTR_OTEL_SCOPE_VERSION } from '@opentelemetry/semantic-conventions'
 import { ENV } from '~/config'
 
+const instrumented: Record<string, boolean> = {}
+
+/**
+ * Decorator to mark a class as recordable for OpenTelemetry tracing.
+ */
+export function recordableClass(): ClassDecorator {
+  const attributes: Attributes = {
+    [ATTR_OTEL_SCOPE_VERSION]: ENV.APP_VERSION,
+  }
+
+  return (obj: Function) => {
+    const className = obj.name
+    const prototype = obj.prototype
+
+    for (const methodName of Object.getOwnPropertyNames(prototype)) {
+      // Skip the constructor or already instrumented methods
+      if (methodName === 'constructor' || instrumented[methodName]) continue
+
+      const descriptor = Object.getOwnPropertyDescriptor(prototype, methodName)
+
+      // Check if it's actually a function/method
+      if (typeof descriptor?.value !== 'function') continue
+
+      const originalMethod = descriptor.value as Function
+
+      attributes['repository.name'] = className
+      attributes['repository.operation'] = methodName
+
+      // Overwrite the method on the prototype
+      prototype[methodName] = function (...args: unknown[]) {
+        return record(`${className}.${methodName}`, attributes, () =>
+          originalMethod.apply(this, args),
+        )
+      }
+
+      // Mark the method as instrumented
+      instrumented[methodName] = true
+    }
+  }
+}
+
+/**
+ * Records a span for the given function, automatically naming it based on the class and method.
+ *
+ * @param name Span name.
+ * @param attrs Span attributes.
+ * @param fn Callback.
+ * @returns Callback return value.
+ */
+export function record<F extends () => unknown>(
+  name: string,
+  attrs: Attributes,
+  fn: F,
+) {
+  const tracer = trace.getTracer(ENV.APP_NAME)
+  const span = tracer.startSpan(
+    name,
+    {
+      attributes: attrs,
+      kind: SpanKind.CLIENT,
+    },
+    context.active(),
+  )
+
+  try {
+    const result = fn()
+
+    if (result instanceof Promise) {
+      return Promise.resolve(result).then(
+        (value) => {
+          finalizeSpan(span)
+          return value
+        },
+        (error) => {
+          finalizeSpan(span, error)
+          throw error
+        },
+      )
+    }
+
+    finalizeSpan(span)
+
+    return result
+  } catch (error) {
+    finalizeSpan(span, error)
+
+    throw error
+  }
+}
+
+/**
+ * Finalizes the span, setting the status and recording any error.
+ *
+ * Credit: https://github.com/kubiks-inc/otel/blob/595b6ee/packages/otel-drizzle/src/index.ts#L131-L143
+ *
+ * @param span The span to finalize.
+ * @param error The error to record, if any.
+ */
 function finalizeSpan(span: Span, error?: Error | unknown) {
   if (error) {
     const err = error instanceof Error ? error : new Error(String(error))
@@ -19,64 +118,4 @@ function finalizeSpan(span: Span, error?: Error | unknown) {
   }
 
   span.end()
-}
-
-export function recordableClass(): ClassDecorator {
-  return (obj: Function) => {
-    const className = obj.name
-    const prototype = obj.prototype
-
-    for (const methodName of Object.getOwnPropertyNames(prototype)) {
-      // Skip the constructor
-      if (methodName === 'constructor') continue
-
-      const descriptor = Object.getOwnPropertyDescriptor(prototype, methodName)
-
-      // Check if it's actually a function/method
-      if (typeof descriptor?.value !== 'function') continue
-
-      const originalMethod = descriptor.value as Function
-
-      // Overwrite the method on the prototype
-      prototype[methodName] = function (...args: unknown[]) {
-        const tracer = trace.getTracer(ENV.APP_NAME)
-
-        const attributes: Attributes = {
-          [ATTR_OTEL_SCOPE_VERSION]: ENV.APP_VERSION,
-          'repository.name': className,
-          'repository.operation': methodName,
-        }
-
-        const span = tracer.startSpan(`${className}.${methodName}`, {
-          attributes,
-          kind: SpanKind.CLIENT,
-        })
-
-        try {
-          const result = originalMethod.apply(this, args)
-
-          if (result instanceof Promise) {
-            return Promise.resolve(result).then(
-              (value) => {
-                finalizeSpan(span)
-                return value
-              },
-              (error) => {
-                finalizeSpan(span, error)
-                throw error
-              },
-            )
-          }
-
-          finalizeSpan(span)
-
-          return result
-        } catch (error) {
-          finalizeSpan(span, error)
-
-          throw error
-        }
-      }
-    }
-  }
 }
