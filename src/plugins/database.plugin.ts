0a83445a69f653253a785ddebff67684b378f69b
Fery Wardiyanto <ferywardiyanto@gmail.com>
2026-09-19 01:33:54 +0700

    chore: remove debug log
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- src/plugins/database.plugin.ts
+++ src/plugins/database.plugin.ts
@@ -39,16 +39,6 @@ instrumentDrizzleClient(db)
 export type AppDatabase = typeof db
 
 export async function migrate() {
-  logger.debug(
-    {
-      host: ENV.DB_HOST,
-      port: ENV.DB_PORT,
-      username: ENV.DB_USER,
-      database: ENV.DB_NAME,
-    },
-    'DB config',
-  )
-
   try {
     await migrator(db, {
       migrationsFolder: './database/migrations',
